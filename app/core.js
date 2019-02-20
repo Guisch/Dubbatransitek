e91dfd35260652a079936655e800c76fcaa47581
Guisch <Guisch@users.noreply.github.com>
2019-02-20 22:21:49 +0100

    fix zip name

--- app/core.js
+++ app/core.js
@@ -524,7 +524,7 @@ module.exports = function(io, lang, similarSongsOption) {
           }
 
           callback(true, lang.playlist.successfullyDeletedPlaylist)
-          return fs.unlink(path.join(__dirname, '../public/playlists', result.name + '.zip'), function(err) {
+          return fs.unlink(path.join(__dirname, '../public/playlists', result.name.replace(/[^a-z0-9]/gi + '.zip'), function(err) {
             if (err)
               console.log(err);
           });
@@ -549,7 +549,7 @@ module.exports = function(io, lang, similarSongsOption) {
                 }
 
                 callback(true, lang.playlist.successfullyDeletedPlaylist);
-                return fs.unlink(path.join(__dirname, '../public/playlists', result.name + '.zip'), function(err) {
+                return fs.unlink(path.join(__dirname, '../public/playlists', result.name.replace(/[^a-z0-9]/gi + '.zip'), function(err) {
                   if (err)
                     console.log(err);
                 });
@@ -1125,7 +1125,7 @@ module.exports = function(io, lang, similarSongsOption) {
 
             var songFile = infos[i].music_id.file.split('/').pop();
             removeQueue.push(function(next) {
-              zip.removeFileFromZip(path.join(__dirname, '../public/playlists', playlistName + '.zip'), songFile, next);
+              zip.removeFileFromZip(path.join(__dirname, '../public/playlists', playlistName.replace(/[^a-z0-9]/gi + '.zip'), songFile, next);
             });
             infos.splice(i, 1);
 
